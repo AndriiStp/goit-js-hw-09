48e2768e5a027739cdb58e3a3ad2cbac06ed69e8
AndriiStp <115407053+AndriiStp@users.noreply.github.com>
2023-04-23 21:13:14 +0000

    Deploying to gh-pages from @ AndriiStp/goit-js-hw-09@ddda944c8e2b9bb020803e0c05d8885fb9475c5a 🚀

---
+++ 01-color-switcher.d9946c90.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),r=document.querySelector("body");e.addEventListener("click",(()=>{setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)&&e.setAttribute("disabled","disabled")})),t.addEventListener("click",(()=>{clearInterval(changer),e.removeAttribute("disabled","disabled")}));
+//# sourceMappingURL=01-color-switcher.d9946c90.js.map
--- 01-color-switcher.e212b8bf.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.querySelector("body");t.addEventListener("click",(function(){setInterval((function(){a.style.backgroundColor="#".concat(Math.floor(16777214*Math.random()).toString(16).padStart(6,0))}),1e3)&&t.setAttribute("disabled","disabled")})),e.addEventListener("click",(function(){clearInterval(changer),t.removeAttribute("disabled","disabled")}))}();
-//# sourceMappingURL=01-color-switcher.e212b8bf.js.map
